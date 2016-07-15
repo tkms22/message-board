47a27698c4dcdf70e1d1652bc06ce15843bb32f7
masa <mkidoch@gmail.com>
2016-07-15 12:52:41 +0000

    fix commit

--- db/migrate/20160715094414_add_age_to_messages.rb
+++ db/migrate/20160715094414_add_age_to_messages.rb
@@ -1,8 +1,5 @@
 class AddAgeToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :age, :integer
-    validates :games_played, numericality: {
-            only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100
-          }
   end
 end
